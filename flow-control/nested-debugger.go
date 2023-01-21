a08bfd511294a876df5a3cd471fae868b82c73b1
Justin Fulton <jusfulton@unsubstantiatedscript.com>
2023-01-21 14:21:14 -0800

    Nested and Debug

---
+++ flow-control/nested-debugger.go
@@ -0,0 +1,14 @@
+package flow_control
+
+import "fmt"
+
+func NestedDebugger() {
+
+	for i := 1; i <= 10; i++ {
+		fmt.Print("i is ", i)
+		for j := 1; j <= 3; j++ {
+			fmt.Print("   j: ", j)
+		}
+		fmt.Println()
+	}
+}
